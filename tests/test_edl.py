b9d2a60ced1db4ed90833500435173f37ee8565b
Johan Egneblad <johan@egneblad.se>
2015-12-07 14:27:21 +0100

    MINOR: Adding edl render tests

--- tests/test_edl.py
+++ tests/test_edl.py
@@ -1,11 +1,22 @@
 
 import pytest
-from core.conform import read_edl_template
+from core.conform import read_edl_template, render_edl
 
 @pytest.fixture(scope='session')
 def edl_template(request):
     xml = read_edl_template()
     return xml
+
+
+@pytest.fixture(scope='session')
+def edl(request, edl_template):
+    return render_edl(
+        edl_template,
+        source_file = 'example2.mp4',
+        dest_file = '/tmp/file.mp4',
+    )
+
+################################################################################
 
 def test_edl_xmlishness(edl_template):
     xml = edl_template
@@ -18,3 +29,11 @@ def test_source_placeholder(edl_template):
 def test_dest_placeholder(edl_template):
     xml = edl_template
     assert '{{dest_file}}' in xml
+
+def test_source_filename(edl):
+    xml = edl_template
+    assert '<filename>example2.mp4</filename>' in xml
+
+def test_dest_filename(edl):
+    xml = edl_template
+    assert '<filename>/tmp/file.mp4</filename>' in xml
