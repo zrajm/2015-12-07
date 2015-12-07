d8db9b2faeaa32be91ef65364c38618d499a687d
Johan Egneblad <johan@egneblad.se>
2015-12-07 11:17:38 +0100

    Adding file signature test

--- tests/test_path.py
+++ tests/test_path.py
@@ -6,3 +6,10 @@ def test_path():
     path = conform()
     assert path == '/tmp/file.mxf'
     assert os.path.isfile(path)
+
+def test_fileinfo():
+    path = conform()
+    with open(path, 'rb') as f:
+        head = f.read(14)
+        print head
+        assert head == '\x06\x0e\x2b\x34\x02\x05\x01\x01\x0d\x01\x02\x01\x01\x02'
