46857d30d7bd4373ce0bba58fac5b5f8d691fb5b
zrajm <zrajm@klingonska.org>
2015-12-07 14:18:51 +0100

    Tests for EDL file.

---
+++ tests/test_edl.py
@@ -0,0 +1,20 @@
+
+import pytest
+from core.conform import read_edl_file
+
+@pytest.fixture(scope='session')
+def edl_template(request):
+    xml = read_edl_file()
+    return xml
+
+def test_edl_xmlishness(edl_template):
+    xml = edl_template
+    assert xml[:5] == '<?xml'
+
+def test_source_placeholder(edl_template):
+    xml = edl_template
+    assert '{{source_file}}' in xml
+
+def test_dest_placeholder(edl_template):
+    xml = edl_template
+    assert '{{dest_file}}' in xml
