4338343b164ff721b09cc33edbaf222f6e5d16e5
Johan Egneblad <johan@egneblad.se>
2015-12-07 12:25:41 +0100

    Test tear down

--- tests/test_path.py
+++ tests/test_path.py
@@ -4,8 +4,16 @@ from program import conform
 
 
 @pytest.fixture(scope='session')
-def done_conform():
-    return conform()
+def done_conform(request):
+    path = conform()
+
+    def delete_file():
+        os.remove(path)
+
+    request.addfinalizer(delete_file)
+
+    return path
+
 
 ################################################################################
 
@@ -21,5 +29,4 @@ def test_file_is_mxf(done_conform):
     path = done_conform
     with open(path, 'rb') as f:
         head = f.read(14)
-        print head
         assert head == '\x06\x0e\x2b\x34\x02\x05\x01\x01\x0d\x01\x02\x01\x01\x02'
